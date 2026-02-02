5ba1b356b5f0925a0efd94f1ec5d3bd15534b7d0
Maya Thompson <majsant96@hotmail.com>
2026-02-02 08:57:52 +0100

    add dashboard html and create venue modal html

--- src/pages/dashboard/CustomerDashboard.tsx
+++ src/pages/dashboard/CustomerDashboard.tsx
@@ -1,3 +1,13 @@
 export default function CustomerDashboard() {
-  return <div>Customer Dashboard</div>;
+  return (
+    <>
+      <div className="container">
+        {/* Profile details (dynamically generated) */}
+        <div className="customer-details-container"></div>
+
+        {/* Customer bookings/favorites (dynamically generated) */}
+        <div className="customer-management-container"></div>
+      </div>
+    </>
+  );
 }
--- src/pages/dashboard/ManagerDashboard.tsx
+++ src/pages/dashboard/ManagerDashboard.tsx
@@ -1,3 +1,13 @@
 export default function VenueManagerDashboard() {
-  return <div>Venue Manager Dashboard</div>;
+  return (
+    <>
+      <div className="container">
+        {/* Profile details (dynamically generated) */}
+        <div className="manager-details-container"></div>
+
+        {/* Customer bookings/favorites (dynamically generated) */}
+        <div className="manager-management-container"></div>
+      </div>
+    </>
+  );
 }
